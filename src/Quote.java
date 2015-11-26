ae154b69634dba07fc53b821de8a76d655b5952c
Totte <thor.gudj@gmail.com>
2015-11-26 07:53:46 +0100

    GREEN

--- src/Quote.java
+++ src/Quote.java
@@ -2,13 +2,21 @@
 public class Quote {
 
     private String quote;   //Quote by a politiction
+    private String[] list = new String[2];
 
     public Quote(String q, String s, String s2, String s3){
         quote = q;
+        list[0] = s;
+        list[1] = s2;
+        list[2] = s3;
     }
 
     public String getQuote(){
         return quote;
+    }
+
+    public String getPerson(int i){
+        return list[i];
     }
 
 }
