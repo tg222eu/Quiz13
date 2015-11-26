33cad5fcd3ce5cac4b54c0ec9074e0c9bff32044
Totte <thor.gudj@gmail.com>
2015-11-26 07:30:47 +0100

    GREEN

---
+++ src/Quote.java
@@ -0,0 +1,14 @@
+
+public class Quote {
+
+    private String qu;
+
+    public Quote(String q, String s, String s2, String s3){
+        qu = q;
+    }
+
+    public String getQuote(){
+        return qu;
+    }
+
+}
