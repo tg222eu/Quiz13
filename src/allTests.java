a084151de257d6b3ebb6bf104ebc8641724c0ccc
Totte <thor.gudj@gmail.com>
2015-11-26 07:44:57 +0100

    RED

--- src/allTests.java
+++ src/allTests.java
@@ -6,8 +6,17 @@ public class allTests {
 
     @Test
     public void shouldStoreStringInQuote(){
-        Quote q = new Quote("Quote insert here", "Person who told the quote", "another person", "another person2");
+        Quote q = new Quote("Quote insert here", "", "",
+                 "");
         assertEquals(q.getQuote(), "Quote insert here");
+    }
+
+    @Test
+    public void getAllPeopleFromQuote(){
+        Quote q = new Quote("", "Person who told the quote", "A person who did not quote", "An another person who did not quote");
+        assertEquals(q.getPerson(0), "Person who told the quote");
+        assertEquals(q.getPerson(1), "A person who did not quote");
+        assertEquals(q.getPerson(2), "An another person who did not quote");
     }
 
 }
